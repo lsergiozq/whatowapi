76adbc16c82da9ccc7cd66eac9e9f4393f3f5f1a
Luiz Sergio <lsergiozq@gmail.com>
2024-10-03 14:34:01 -0300

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -11,5 +11,11 @@ module.exports = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: false
+  logging: false,
+  pool: {
+    max: 15,        // Aumentar número máximo de conexões
+    min: 0,
+    acquire: 60000, // Aumentar o tempo de aquisição para 1 minuto
+    idle: 30000     // Aumentar tempo ocioso para 30 segundos
+  }
 };
