40d453bf101f825208be1d3781551b161acfa4e6
Luiz Sergio <lsergiozq@gmail.com>
2024-10-07 19:08:55 -0300

    Update GetWhatsAppByIdClient.ts

--- backend/src/helpers/GetWhatsAppByIdClient.ts
+++ backend/src/helpers/GetWhatsAppByIdClient.ts
@@ -3,8 +3,19 @@ import Whatsapp from "../models/Whatsapp";
 
 const GetWhatsAppByIdClient = async (idclient: string): Promise<Whatsapp> => {
 
+  //verifica se process.env.API_ID possui valor, caso não atribui 1
+  if(!process.env.API_ID){
+    process.env.API_ID = '1';
+  }
+
+  //converte process.env.API_ID para inteiro
+  const apiId = parseInt(process.env.API_ID || '1', 10);
+
   const defaultWhatsapp = await Whatsapp.findOne({
-    where: { idclient: idclient }
+    where: { 
+      idclient: idclient,
+      idapi: apiId
+    }
   });
 
   if (!defaultWhatsapp) {
