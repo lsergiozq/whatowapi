157582a522b2bd4a15b78bcf4b44fa5e92b1a9c2
Luiz Sergio <lsergiozq@gmail.com>
2024-10-08 11:38:32 -0300

    Update ApiMessagesController.ts

--- backend/src/controllers/ApiMessagesController.ts
+++ backend/src/controllers/ApiMessagesController.ts
@@ -10,6 +10,7 @@ import DeleteWhatsAppService from "../services/WhatsappService/DeleteWhatsAppSer
 import SendWhatsAppMedia from "../helpers/SendWhatsAppMedia";
 import * as fs from 'fs/promises'; // Certifique-se de usar a versão do fs que suporta Promises
 import Queue from 'bull';
+import Redis from 'ioredis'; // Use ioredis para verificação do status
 
 // Configuração da fila com estratégia de reconexão
 const messageQueue = new Queue(`messageQueue${process.env.API_ID}`, {
@@ -23,6 +24,21 @@ const messageQueue = new Queue(`messageQueue${process.env.API_ID}`, {
   }
 });
 
+// Verificação do estado do Redis
+const redisClient = new Redis(); // Conectar ao Redis para verificar seu status
+
+const checkRedisReady = async (): Promise<void> => {
+  try {
+    const info = await redisClient.info();
+    if (info.includes("loading:1")) {
+      throw new Error("Redis is still loading the dataset into memory.");
+    }
+  } catch (error) {
+    console.error('Erro ao verificar o status do Redis:', error);
+    throw error;
+  }
+};
+
 // Lidar com erro de conexão com o Redis
 messageQueue.on('error', (error) => {
   console.error('Erro de conexão com o Redis:', error);
@@ -35,9 +51,10 @@ messageQueue.on('stalled', (job) => {
 
 // Definir o processador da fila com tratamento de erros
 messageQueue.process(async (job, done) => {
-  const { whatsapp, number, body, media } = job.data;
-
   try {
+    await checkRedisReady(); // Verifique se o Redis está pronto antes de processar
+    const { whatsapp, number, body, media } = job.data;
+
     if (media) {
       await SendWhatsAppMedia({ whatsapp, media, body, number });
       await fs.unlink(media.path);
@@ -59,6 +76,8 @@ export const index = async (req: Request, res: Response): Promise<Response> => {
   newContact.number = newContact.number.replace("-", "").replace(" ", "");
 
   try {
+    await checkRedisReady(); // Verifique se o Redis está pronto antes de adicionar à fila
+
     const whatsapp = await GetWhatsAppByName(newContact.idclient);
 
     // Adiciona as mensagens na fila com tratamento de promessas
