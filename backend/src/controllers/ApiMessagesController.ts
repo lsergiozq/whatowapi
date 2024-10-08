28c49ccbdae7ad0d6063947b90dc671eafabc031
Luiz Sergio <lsergiozq@gmail.com>
2024-10-08 12:33:39 -0300

    Update ApiMessagesController.ts

--- backend/src/controllers/ApiMessagesController.ts
+++ backend/src/controllers/ApiMessagesController.ts
@@ -11,7 +11,7 @@ import SendWhatsAppMedia from "../helpers/SendWhatsAppMedia";
 import * as fs from 'fs/promises'; // Certifique-se de usar a versão do fs que suporta Promises
 import Queue from 'bull';
 
-const messageQueue = new Queue(`messageQueue${process.env.API_ID}`, {
+const messageQueue = new Queue(`messageQueue`, {
   redis: { host: '127.0.0.1', port: 6379 },
 });
 
@@ -33,24 +33,6 @@ interface SessionData {
   key: string;
 }
 
-// Função para verificar se o Redis está pronto com lógica de repetição
-const checkRedisReady = async (queue: Queue.Queue, retries = 5, delay = 1000): Promise<void> => {
-  for (let i = 0; i < retries; i++) {
-    try {
-      const result = await queue.client.ping();
-      if (result === 'PONG') {
-        return;
-      }
-    } catch (err) {
-      if (i === retries - 1) {
-        throw err;
-      }
-    }
-    await new Promise(resolve => setTimeout(resolve, delay));
-  }
-  throw new Error('Redis is not ready');
-};
-
 // Definir o processador da fila
 messageQueue.process(async (job, done) => {
   const { whatsapp, number, body, media } = job.data;
@@ -70,39 +52,36 @@ messageQueue.process(async (job, done) => {
 });
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
-  try {
-    const newContact: ContactData = req.body;
-    const messageData: MessageData = req.body;
-    const medias = req.files as Express.Multer.File[];
+  const newContact: ContactData = req.body;
+  const messageData: MessageData = req.body;
+  const medias = req.files as Express.Multer.File[];
 
-    newContact.number = newContact.number.replace("-", "").replace(" ", "");
+  newContact.number = newContact.number.replace("-", "").replace(" ", "");
 
-    const whatsapp = await GetWhatsAppByName(newContact.idclient);
+  const whatsapp = await GetWhatsAppByName(newContact.idclient);
 
-    // Verifica se o Redis está pronto antes de adicionar trabalhos à fila
-    await checkRedisReady(messageQueue);
-
-    // Adiciona as mensagens na fila
-    if (medias && medias.length > 0) {
-      for (const media of medias) {
-        await messageQueue.add({
-          whatsapp,
-          number: newContact.number,
-          body: messageData.body,
-          media,
-        });
-      }
-    } else {
-      await messageQueue.add({
+  // Adiciona as mensagens na fila
+  if (medias && medias.length > 0) {
+    medias.forEach((media: Express.Multer.File) => {
+      messageQueue.add({
         whatsapp,
         number: newContact.number,
         body: messageData.body,
+        media,
       });
-    }
-
-    return res.send();
-  } catch (error) {
-    console.error('Erro ao processar a requisição:', error);
-    return res.status(500).send('Erro ao processar a requisição');
+    });
+  } else {
+    messageQueue.add({
+      whatsapp,
+      number: newContact.number,
+      body: messageData.body,
+    });
   }
+
+  return res.send();
 };
+
+
+
+
+
