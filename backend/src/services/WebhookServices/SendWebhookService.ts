06369c3b56f1c7fcac37f48d83c103fa640384e2
Luiz Sergio <lsergiozq@gmail.com>
2024-10-11 10:57:23 -0300

    Update SendWebhookService.ts

--- backend/src/services/WebhookServices/SendWebhookService.ts
+++ backend/src/services/WebhookServices/SendWebhookService.ts
@@ -1,22 +1,23 @@
 import axios from "axios";
-import FormData from "form-data";
+import qs from "qs";  // Importa a biblioteca qs para formatar os dados como x-www-form-urlencoded
 
 class WebhookService {
   public async send(payload: any, retryCount = 0): Promise<void> {
     const webhookUrl = process.env.WEBHOOK_URL || "http://example.com/webhook";
     const maxRetries = 3;
       
-    // Cria um novo form-data
-    const formData = new FormData();
-    formData.append("TID", "1234");
-    formData.append("NSU", "payload");
+    //temporário
+    payload = { TID: '123456', NSU: '654321' };
+
+    // Converte o payload para o formato x-www-form-urlencoded
+    const data = qs.stringify(payload);
 
     try {
       console.log("Enviando para o webhook:" + webhookUrl, payload);
-      await axios.post(webhookUrl, formData, {
+      await axios.post(webhookUrl, data, {
         headers: {
-          ...formData.getHeaders(), // Inclui os headers apropriados para o form-data
-        },
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       });
     } catch (error) {
       console.error(`Erro ao enviar para o webhook: ${error.message}`);
