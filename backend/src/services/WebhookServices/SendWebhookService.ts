b54f37179cc27d5cedf2636ff80db61e1b462efc
Luiz Sergio <lsergiozq@gmail.com>
2024-10-11 10:50:45 -0300

    Inclusão do formdata

--- backend/src/services/WebhookServices/SendWebhookService.ts
+++ backend/src/services/WebhookServices/SendWebhookService.ts
@@ -1,19 +1,23 @@
 import axios from "axios";
+import FormData from "form-data";
 
 class WebhookService {
   public async send(payload: any, retryCount = 0): Promise<void> {
     const webhookUrl = process.env.WEBHOOK_URL || "http://example.com/webhook";
     const maxRetries = 3;
-
-    payload = {
-        authorizationId: "abc123def456",
-        referenceId: "ref789ghi012"
-    };
       
+    // Cria um novo form-data
+    const formData = new FormData();
+    formData.append("TID", "1234");
+    formData.append("NSU", payload);
 
     try {
       console.log("Enviando para o webhook:" + webhookUrl, payload);
-      await axios.post(webhookUrl, payload);
+      await axios.post(webhookUrl, formData, {
+        headers: {
+          ...formData.getHeaders(), // Inclui os headers apropriados para o form-data
+        },
+      });
     } catch (error) {
       console.error(`Erro ao enviar para o webhook: ${error.message}`);
 
